b10046f6679fe2badbfe6225ac5e698f4546ef3e
Hugo Cardoso <48107335+Abjiri@users.noreply.github.com>
2020-11-18 11:28:44 +0000

    documentação do código e remoção alunos inválidos do dataset

--- T5/api.js
+++ T5/api.js
@@ -1,11 +1,13 @@
 var axios = require('axios');
 
+//mensagem de pedido inválido na frontend
 function badRequest(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
     res.write(`<p>Pedido não suportado: ${req.method} ${req.url}</p>`);
     res.end();
 }
 
+//mensagem de página não encontrada, frontend e backend
 function pageNotFound(collection, page, res) {
     console.log(`A página ${page} de ${collection} não existe!`);
 
@@ -15,6 +17,7 @@ function pageNotFound(collection, page, res) {
     footer(res);
 }
 
+//índice principal
 function index(res) {
     res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
     res.write('<h2>Escola de Música</h2>');
@@ -42,6 +45,7 @@ function getLinks(data) {
     return links;
 }
 
+//informação da página de estudante
 function studentInfo(s, res) {
     res.write(`<p><b>Número mecanográfico:</b> ${s.id}</p>`);
     res.write(`<p><b>Nome:</b> ${s.nome}</p>`);
@@ -51,6 +55,7 @@ function studentInfo(s, res) {
     res.write(`<p><b>Instrumento:</b> ${s.instrumento}</p>`);
 }
 
+//informação da página de curso
 function courseInfo(c, res) {
     res.write(`<p><b>Identificador:</b> ${c.id}</p>`);
     res.write(`<p><b>Designação:</b> ${c.designacao}</p>`);
@@ -58,22 +63,25 @@ function courseInfo(c, res) {
     res.write(`<p><b>Instrumento:</b> <a href="/instrumentos/${c.instrumento.id}">${c.instrumento['#text']}</a></p>`);
 }
 
+//informação da página de instrumento
 function instrumentInfo(i, res) {
     res.write(`<p><b>Identificador:</b> ${i.id}</p>`);
     res.write(`<p><b>Designação:</b> ${i['#text']}</p>`);
 }
 
+//rodapé comum à maior parte das páginas, com referência para o índice principal
 function footer(res) {
     res.write(`<p><address>[ <a href="/">Início</a> ]</address></p>`);
     res.end();
 }
 
+//página com elementos de uma certa coleção
 function getCollectionIndex(collection, req, res, page) {
-    var sort = collection == 'cursos' ? '&_sort=id' : '';
+    var sort = collection == 'cursos' ? '&_sort=id' : ''; //o caso dos cursos é o único qu não está já ordenado por ordem alfabética
 
-    axios.get('http://localhost:3001/' + collection + '?_page=' + page + sort)
+    axios.get('http://localhost:3001/' + collection + '?_page=' + page + sort) //pedido para o json-server
     .then(resp => {
-        if (!resp.data.length) pageNotFound(collection, page, res);
+        if (!resp.data.length) pageNotFound(collection, page, res); //se não tiver elementos, a página é inválida
         else {
             res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
             res.write(`<h2>Escola de Música: ${collection.charAt(0).toUpperCase()+collection.slice(1)}</h2>`);
@@ -83,6 +91,7 @@ function getCollectionIndex(collection, req, res, page) {
                 let e = resp.data[i];
 
                 var li;
+                //imprimir os elementos da coleção em questão
                 switch(collection) {
                     case 'alunos': li = `<li><a href="/alunos?_page=${page}/${e.id}">${e.id} - ${e.nome}</a></li>`; break;
                     case 'cursos': li = `<li><a href="/cursos?_page=${page}/${e.id}">${e.id} - ${e.designacao}</a></li>`; break;
@@ -92,11 +101,14 @@ function getCollectionIndex(collection, req, res, page) {
             }
             res.write('</ul>');
 
+            //ir buscar os links para as páginas relevantes da coleção
             let links = getLinks(resp.headers['link']),
+                first = `<a href="${links['first']}">Primeira página</a>`,
                 prev = links['prev'] ? `<a href="${links['prev']}">Anterior</a>` : '-',
-                next = links['next'] ? `<a href="${links['next']}">Seguinte</a>` : '-';
+                next = links['next'] ? `<a href="${links['next']}">Seguinte</a>` : '-',
+                last = `<a href="${links['last']}">Última página</a>`;
 
-            res.write(`<p>[ ${prev} | ${next} ]</p>`);
+            res.write(`<p>[ ${first} | ${prev} | ${next} | ${last} ]</p>`);
 
             footer(res);
         }
@@ -107,24 +119,30 @@ function getCollectionIndex(collection, req, res, page) {
     });
 }
 
+//página com informação de um elemento da coleção
 function getCollectionElem(url, res) {
+    //extrai a coleção em questão e o id do elemento do url
     var split = url.split('/'),
         collection = split[1].includes('?') ? split[1].split('?')[0] : split[1],
         id = split[2];
 
-    axios.get(`http://localhost:3001/${collection}/${id}`)
+    axios.get(`http://localhost:3001/${collection}/${id}`) //pedido para o json-server
         .then(resp => {
             res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
             
+            //imprimir informação do elemento em questão
             if (collection == 'alunos') studentInfo(resp.data, res);
             if (collection == 'cursos') courseInfo(resp.data, res);
             if (collection == 'instrumentos') instrumentInfo(resp.data, res);
 
             res.write('<br>');
+            -
+            //link para a coleção em questão (e a página em específico deste element, se estiver no link)
             res.write(`<p><address>[ <a href="/${split[1]}">Lista de ${collection}</a> ]</address>`);
             footer(res);
         })
         .catch(error => {
+            //mensagem de erro de obtenção de elemento com id inválido, frontend e backend
             var elem = collection.slice(0,-1);
 
             console.log(`Erro na obtenção do ${elem} ${id}: error`);
--- T5/server.js
+++ T5/server.js
@@ -8,18 +8,22 @@ http.createServer(function (req,res) {
         if (req.url == '/') {
             api.index(res);
         }
+        //urls básicos das coleções
         else if (req.url.match(/^\/(alunos|cursos|instrumentos)\/?$/)) {
             api.getCollectionIndex(req.url.split('/')[1], req, res, 1);
         }
+        //urls de páginas das coleções
         else if (req.url.match(/^\/(alunos|cursos|instrumentos)\?_page=[1-9][0-9]*\/?$/)) {
             let page = req.url.split('=')[1];
             api.getCollectionIndex(req.url.split('/')[1].split('?')[0], req, res, page);
         }
+        //urls básicos de elementos das coleções
         else if (req.url.match(/^\/alunos\/A[0-9]*\/?$/)
               || req.url.match(/^\/cursos\/C[BS][0-9]*\/?$/)
               || req.url.match(/^\/instrumentos\/I[0-9]*\/?$/)) {
             api.getCollectionElem(req.url,res,1);
         }
+        //urls de elementos das coleções com a página da coleção em que se encontram
         else if (req.url.match(/^\/alunos\?_page=[1-9][0-9]*\/A[0-9]*\/?$/)
               || req.url.match(/^\/cursos\?_page=[1-9][0-9]*\/C[BS][0-9]*\/?$/)
               || req.url.match(/^\/instrumentos\?_page=[1-9][0-9]*\/I[0-9]*\/?$/)) {
@@ -27,10 +31,12 @@ http.createServer(function (req,res) {
             api.getCollectionElem(req.url,res,page);
         }
         else {
+            //outros urls aos quais este servidor não responde
             api.badRequest(req,res);
         }
     }
     else {
+        //outros tipos de pedidos aos quais este servidor não responde
         api.badRequest(req,res);
     }
 }).listen(4000);
